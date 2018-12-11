d05e91415bd1dc938a21b3af4182c9f93ad2cfaa
Jiajun Yan <domo36k@gmail.com>
2018-12-11 09:41:18 +0800

    add font awesome and complete styling of index

--- src/components/index/index.jsx
+++ src/components/index/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const IndexComponent = props => {
   const { state } = props;
 
-  return <div>123</div>;
+  return (
+    <div className="index">
+      <h2>{state.content.title}</h2>
+      {state.content.content.map((item, index) => {
+        return <p key={index}>{item}</p>;
+      })}
+
+      <div className="index__icons">
+        {state.links.map((item, index) => {
+          return (
+            <a href={item.href} key={index}>
+              <FontAwesomeIcon icon={item.icon} size="lg" />
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 IndexComponent.propTypes = {
--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import bindAll from 'lodash.bindall';
 import IndexComponent from '../components/index/index';
+import { faMediumM, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 class Index extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: {
+        title: 'A Warm Welcome.',
+        content: [
+          'Hey, I am Jiajun Yan, a junior Web Developer currently based in Guangzhou, China with strong interests in React, Vue and JavaScript performance.',
+          'Graduated from Cardiff University, Wales with a Master’s degree in Computing (Distinction), where I found myself truly passionate about Web Technologies, travel and making friends.',
+          'I am active on social media and also in love with writing blog posts and taking notes. Feel free to ping me on any of the following platforms below.'
+        ]
+      },
+      links: [
+        {
+          href: 'https://medium.com/@realfrancisyan',
+          icon: faMediumM
+        },
+        {
+          href: 'https://www.instagram.com/jiajun.yan.travel',
+          icon: faInstagram
+        },
+        {
+          href: 'https://www.instapaper.com/p/realfrancisyan',
+          icon: faBook
+        }
+      ]
+    };
 
     bindAll(this, []);
   }

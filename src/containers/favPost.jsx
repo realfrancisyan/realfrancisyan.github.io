35fb06365254dc45d610dc519f6ae14681007ce2
Jiajun Yan <yanjiajun@nome.com>
2019-09-05 23:10:27 +0800

    Fixed menu and favorite posts

--- src/containers/favPost.jsx
+++ src/containers/favPost.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import bindAll from 'lodash.bindall';
 import FavPostComponent from '../components/favPost';
-import {getList} from '../api/favorites'
+import { getList, getAppsInfo } from '../api/favorites'
 import constants from '../common/constants'
 
 class FavPost extends Component {
@@ -10,10 +10,13 @@ class FavPost extends Component {
 
     this.state = {
       postType: null,
-      postList: []
+      postList: [],
+      imageIsLoaded: false
     };
 
     bindAll(this, [
+      'handleExpandDesc',
+      'handleImageLoaded'
     ])
   }
 
@@ -22,11 +25,21 @@ class FavPost extends Component {
 
     const onSuccess = res => {
       if (res.result === constants.STATUS.SUCCESS) {
-        console.log(res)
         this.setState({
           postType,
           postList: res.data
         })
+
+        // if post type is 0(ios apps), then request Apple for details
+        if (+postType === 0) {
+          const appIds = []
+          res.data.forEach(item => {
+            appIds.push(item.appId)
+            item.expandDesc = false
+          })
+  
+          this.handleGetAppsInfo(appIds)
+        }
       }
     }
 
@@ -39,12 +52,59 @@ class FavPost extends Component {
       })
   }
 
+  handleGetAppsInfo(ids) {
+    const onSuccess = res => {
+      const { postList } = this.state
+      res.results.forEach((item, index) => {
+        postList[index].title = item.trackCensoredName
+        postList[index].body = item.description
+        postList[index].appIcon = item.artworkUrl512
+        postList[index].appLink = item.trackViewUrl
+
+        Object.assign(postList[index], item)
+      })
+
+      this.setState({
+        postList
+      })
+    }
+    getAppsInfo({ id: ids.join() })
+      .then(onSuccess)
+      .catch(err => {
+        console.error('request get apps info failed - ', err);
+      })
+  }
+
+  handleExpandDesc(item) {
+    const {postList} = this.state
+    postList.forEach(i => {
+      if (i.trackName === item.trackName) {
+        i.expandDesc = !i.expandDesc
+      }
+    })
+    this.setState({
+      postList
+    })
+  }
+
+  handleImageLoaded() {
+    this.setState({
+      imgIsLoaded: true
+    })
+  }
+
   componentWillMount() {
     this.handleGetPosts()
   }
 
   render() {
-    return <FavPostComponent state={this.state} />;
+    return (
+      <FavPostComponent 
+        state={this.state} 
+        handleExpandDesc={this.handleExpandDesc}
+        handleImageLoaded={this.handleImageLoaded}
+      />
+    )
   }
 }
 
